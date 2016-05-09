129547ad63ee4d1a03fb6e2b2c18b9b487f62d9f
Administrator <Administrator@LA8RF477A4GCWAU>
2016-05-09 19:16:06 +0800

    添加项目文件。

---
+++ HungryHero/src/Scene/MenuScene.js
@@ -0,0 +1,31 @@
+﻿var MenuScene = cc.Scene.extend({
+    ctor: function () {
+
+        this._super();
+        var layer = new cc.Layer();
+        this.addChild(layer);
+
+        var winSize = cc.winSize;
+
+        var bgWelcome = new cc.Sprite(res.welcomeBG);
+        bgWelcome.x = winSize.width / 2;
+        bgWelcome.y = winSize.height / 2;
+        layer.addChild(bgWelcome);
+
+
+        var title = new cc.Sprite("#welcome_title.png");
+        title.x = 800;
+        title.y = 500;
+        layer.addChild(title);
+
+        var hero = new cc.Sprite("#welcome_hero.png");
+        hero.x = 300;
+        hero.y = 400;
+        layer.addChild(hero);
+    }
+
+
+
+
+
+});
---
+++ HungryHero/src/resource.js
@@ -0,0 +1,10 @@
+var res = {
+    welcomeBG : "res/graphics/bgWelcome.jpg",
+    textureJpg: "res/graphics/texture.jpg",
+    texturePlist: "res/graphics/texture.plist",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
